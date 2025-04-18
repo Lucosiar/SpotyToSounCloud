feeb2f0e764396b75491f3a8aff6b4508e60e60f
Lucosiar <lucosiar333@gmail.com>
2025-04-18 14:12:46 +0200

    Versión inicial de config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# Spotify
+# Para conseguir estas credenciales, debes crear una aplicación en Spotify
+# URL: https://developer.spotify.com/dashboard  
+
+# 1. Inicia sesión en Spotify
+# 2. Crea una aplicación
+# 3. Rellena el app name y la descripción con el ejemplo que quieras 
+# 4. Rellena el redirect URI con la uri de debajo:
+# 5. Acepta terminos y condiciones
+# 6. Copia el client id y el client secret
+
+# Spotify
+SPOTIFY_CLIENT_ID = "tu client id"
+SPOTIFY_CLIENT_SECRET = "tu clietn secret"
+SPOTIFY_REDIRECT_URI = "la url" # Yo use: http://127.0.0.1:8888/callback
+
+# SoundCloud
+# Para conseguir estas credenciales, debes crear una aplicación en SoundCloud
+SOUNDCLOUD_CLIENT_ID = "tu client id"
+SOUNDCLOUD_CLIENT_SECRET = "tu client secret"
+SOUNCLOUD_REDIRECT_URI = "la url"
